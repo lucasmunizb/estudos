23d378dea8ddaad23af411c70c58784c94b7f002
Lucas Muniz <lucasmunizbraga@gmail.com>
2023-01-21 14:01:59 -0300

    add

---
+++ v1/index.php
@@ -0,0 +1,10 @@
+<?php
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+
+require dirname(__DIR__).'/composer/vendor/autoload.php';
+
+echo "<pre>";
+var_dump(__DIR__);
+echo "</pre>";
+die();

df47ed52a873ed1bdce909f070028c7719560c9f
Lucas Muniz <lucasmunizbraga@gmail.com>
2023-01-22 11:36:30 -0300

    Atualizando arquivo de rotas e printando retorno da request uri

--- v1/Classes/Rota.php
+++ v1/Classes/Rota.php
@@ -1,11 +1,17 @@
 <?php
-
 namespace Classes;
 
 class Rota
 {
-    public static function getUrl()
+    public function __construct()
     {
-        return $_SERVER['REQUEST_URI'];
+        echo "<pre>";
+        var_dump(self::getUrl());
+        echo "</pre>";
+        die();
+    }
+    public function getUrl():string
+    {
+        return parse_url($_SERVER['REQUEST_URI'],PHP_URL_PATH);
     }
 }

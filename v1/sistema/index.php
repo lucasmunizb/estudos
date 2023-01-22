7d89452b1eff118051c6c1c863615c676010a252
Lucas Muniz <lucasmunizbraga@gmail.com>
2023-01-22 11:33:59 -0300

    iniciando a index e criando instancia do arquivo de Rotas

--- v1/sistema/index.php
+++ v1/sistema/index.php
@@ -1,10 +1,12 @@
 <?php
 
-use Classes\Rota;
-
 ini_set('display_errors',1);
 error_reporting(E_ALL);
 
 require_once dirname(__DIR__).'/vendor/autoload.php';
 
-$Rota = new Rota;
+use Classes\Rota;
+
+$Rota = new Rota();
+
+

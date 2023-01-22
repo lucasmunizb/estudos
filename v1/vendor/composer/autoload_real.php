dfd8e0a3893e011ded604e7c288ce2cc42f31e4e
Lucas Muniz <lucasmunizbraga@gmail.com>
2023-01-22 11:18:29 -0300

    Instalacao composer e Guzzle

--- v1/vendor/composer/autoload_real.php
+++ v1/vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit5dec06bf0081a71c0e196e5e130c2a22
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit5dec06bf0081a71c0e196e5e130c2a22', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit5dec06bf0081a71c0e196e5e130c2a22', 'loadClassLoader'));
@@ -30,6 +32,18 @@ class ComposerAutoloaderInit5dec06bf0081a71c0e196e5e130c2a22
         call_user_func(\Composer\Autoload\ComposerStaticInit5dec06bf0081a71c0e196e5e130c2a22::getInitializer($loader));
 
         $loader->register(true);
+
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit5dec06bf0081a71c0e196e5e130c2a22::$files;
+        $requireFile = static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        };
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            ($requireFile)($fileIdentifier, $file);
+        }
 
         return $loader;
     }

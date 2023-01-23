5065452757c9aef77797f0a8d642c8ab0f79a12a
Lucas Muniz <lucasmunizbraga@gmail.com>
2023-01-22 21:51:04 -0300

    tratamento para usar o parametro da url

--- v1/sistema/bancos.php
+++ v1/sistema/bancos.php
@@ -4,7 +4,8 @@ use GuzzleHttp\Exception\GuzzleException;
 $client = new Client(['base_uri' => 'https://brasilapi.com.br']);
 
 try {
-    $response = $client->request('GET', '/api/banks/v1');
+    $parametro = $Rota->getParametro();
+    $response = $client->request('GET', "/api/banks/v1/$parametro");
     echo "<pre>";
     print_r(json_decode($response->getBody()));
     echo "</pre>";

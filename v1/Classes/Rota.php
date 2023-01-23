587513e400f7988eae9d7518a30a4d90be3a6b47
Lucas Muniz <lucasmunizbraga@gmail.com>
2023-01-22 21:50:40 -0300

    Definindo construtor e alimentando variavel de parametro e arquivo

--- v1/Classes/Rota.php
+++ v1/Classes/Rota.php
@@ -1,17 +1,41 @@
 <?php
 namespace Classes;
-
+define('ROTA_V1',dirname(__DIR__));
 class Rota
 {
+    private string $parametro;
+    private string $arquivo;
+
     public function __construct()
     {
-        echo "<pre>";
-        var_dump(self::getUrl());
-        echo "</pre>";
-        die();
+        $rotaInicial = explode('sistema/',self::getUrl())[1];
+        $rota = explode('/',$rotaInicial);
+        if (!file_exists(dirname(__DIR__)."/sistema/$rota[0].php")) {
+            echo "404";
+            exit;
+        }
+        self::setArquivo(ROTA_V1."/sistema/$rota[0].php");
+        $parametro = end($rota) != $rota[0] ? end($rota) : '';
+        self::setParametro($parametro);
     }
     public function getUrl():string
     {
         return parse_url($_SERVER['REQUEST_URI'],PHP_URL_PATH);
+    }
+    public function getArquivo(): string
+    {
+        return $this->arquivo;
+    }
+    public function setArquivo(string $arquivo): void
+    {
+        $this->arquivo = $arquivo;
+    }
+    public function getParametro(): string
+    {
+        return $this->parametro;
+    }
+    public function setParametro(string $parametro): void
+    {
+        $this->parametro = $parametro;
     }
 }

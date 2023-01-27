7fc99c0cb46ba70fd031638d29486b610099630f
Lucas Muniz <lucasmunizbraga@gmail.com>
2023-01-26 22:38:43 -0300

    add v2

---
+++ v2/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'lucas/v2',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '62dd6c2d924e4af998785c2b05fda463e7eda6a8',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'lucas/v2' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '62dd6c2d924e4af998785c2b05fda463e7eda6a8',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
